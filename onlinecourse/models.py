0dc48fd72cf0d18338466bec4342375c0ba484f3
vikiru <72267229+vikiru@users.noreply.github.com>
2023-10-30 18:20:53 -0400

    Commit changes following lab instructions manual

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -5,10 +5,8 @@ try:
 except Exception:
     print("There was an error loading django modules. Do you have django installed?")
     sys.exit()
-
 from django.conf import settings
 import uuid
-
 
 # Instructor model
 class Instructor(models.Model):
@@ -18,11 +16,9 @@ class Instructor(models.Model):
     )
     full_time = models.BooleanField(default=True)
     total_learners = models.IntegerField()
-
     def __str__(self):
         return self.user.username
-
-
+    
 # Learner model
 class Learner(models.Model):
     user = models.ForeignKey(
@@ -46,12 +42,10 @@ class Learner(models.Model):
         default=STUDENT
     )
     social_link = models.URLField(max_length=200)
-
     def __str__(self):
         return self.user.username + "," + \
                self.occupation
-
-
+               
 # Course model
 class Course(models.Model):
     name = models.CharField(null=False, max_length=30, default='online course')
@@ -62,20 +56,17 @@ class Course(models.Model):
     users = models.ManyToManyField(settings.AUTH_USER_MODEL, through='Enrollment')
     total_enrollment = models.IntegerField(default=0)
     is_enrolled = False
-
     def __str__(self):
         return "Name: " + self.name + "," + \
                "Description: " + self.description
-
-
+               
 # Lesson model
 class Lesson(models.Model):
     title = models.CharField(max_length=200, default="title")
     order = models.IntegerField(default=0)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     content = models.TextField()
-
-
+    
 # Enrollment model
 # <HINT> Once a user enrolled a class, an enrollment entry should be created between the user and course
 # And we could use the enrollment to track information such as exam submissions
@@ -93,11 +84,26 @@ class Enrollment(models.Model):
     date_enrolled = models.DateField(default=now)
     mode = models.CharField(max_length=5, choices=COURSE_MODES, default=AUDIT)
     rating = models.FloatField(default=5.0)
-
-
-# One enrollment could have multiple submission
-# One submission could have multiple choices
-# One choice could belong to multiple submissions
-#class Submission(models.Model):
-#    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
-#    choices = models.ManyToManyField(Choice)
+    
+class Question(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    content = models.CharField(max_length=200)
+    grade = models.IntegerField(default=50)
+    def __str__(self):
+        return "Question: " + self.content
+    def is_get_score(self, selected_ids):
+        all_answers = self.choice_set.filter(is_correct=True).count()
+        selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()
+        if all_answers == selected_correct:
+            return True
+        else:
+            return False
+        
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    content = models.CharField(max_length=200)
+    is_correct = models.BooleanField(default=False)
+    
+class Submission(models.Model):
+    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
+    choices = models.ManyToManyField(Choice)
